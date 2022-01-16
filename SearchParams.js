bc0c800cfd78df6a2e34e5d72f7d202fc63adf03
yix-shu <yix.shu723@gmail.com>
2022-01-15 18:28:48 -0800

    Created breed selection so that breed value is updated and retrieved through a hook

--- SearchParams.js
+++ SearchParams.js
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import Pet from "./Pet";
 
 const SearchParams = () => {
     const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
@@ -12,13 +13,20 @@ const SearchParams = () => {
     const [animal, updateAnimal] = useState("");
     const [breed, updateBreed] = useState("");
     const breeds = [];
+    const [pets, setPets] = useState([]);
+
 
     useEffect(() => {
         requestPets();
     }, [])
 
     async function requestPets(){
-        
+        const res = await fetch(
+            `http://pets-v2.dev-apis.com/pet?animal=${animal}&location=${location}&breed=${breed}`
+        );
+        const json = await res.json();
+        setPets(json.pets);
+
     }
 
     return (
@@ -46,8 +54,30 @@ const SearchParams = () => {
                 }
               </select>
           </label>
+          <label htmlFor = "breed">
+              Breed
+              <select
+                disabled = {!breeds.length}
+                id = "breed"
+                value = {breed}
+                onChange={(e) => {updateBreed(e.target.value)}}
+                onBlur = {(e) => {updateBreed(e.target.value)}}
+              >
+                <option />
+                {
+                    breeds.map((breed) => (
+                        <option key = {breed} value = {breed}>
+                            {breed}
+                        </option>
+                    ))
+                }
+              </select>
+          </label>
           <button>Submit</button>
         </form>
+        {
+
+        }
       </div> 
     );
   };
