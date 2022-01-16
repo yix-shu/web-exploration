71af6a7d95226a11396e14f7d9eff62a432aaf23
yix-shu <yix.shu723@gmail.com>
2022-01-15 18:49:15 -0800

    Fixed errors

--- Pet.js
+++ Pet.js
@@ -18,14 +18,14 @@ export default function Pet({props}) {
     
 }
 */
-const Pet = (props) => (
+const Pet = (props) => {
     return (
         <div>
             <h1>(props.name)</h1>
             <h2>(props.animal)</h2>
             <h3>(props.breed)</h3>
         </div>
-    );
-);
+    )
+};
 
 export default Pet;
