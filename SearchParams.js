c99f31257a8b95d6c9f5074e08db61bc5fd21d7c
yix-shu <yix.shu723@gmail.com>
2022-01-15 18:23:16 -0800

    updated with animal selection an breeds and used hooks

--- SearchParams.js
+++ SearchParams.js
@@ -1,16 +1,50 @@
 import {useState} from "react";
 
 const SearchParams = () => {
+    const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
+    
     //const location = "Seattle, WA"; //this made it so you couldn't change the location
     const [location, updateLocation] = useState("Seattle, Washington"); //sets Seattle by default but this is modifiable
     updateLocation("Toronto, ON"); //cannot update variable directly. Must use function bc it is by React's design
     //cannot do the following: location = "Toronto, ON";
+    console.log(location) //checks to see if changed location is registered
+    const [animal, updateAnimal] = useState("");
+    const [breed, updateBreed] = useState("");
+    const breeds = [];
+
+    useEffect(() => {
+        requestPets();
+    }, [])
+
+    async function requestPets(){
+        
+    }
+
     return (
       <div className="search-params">
         <form>
           <label htmlFor="location">
             Location
             <input id="location" value={location} placeholder="Location" onChange{...(e) => {updateLocation(e.target.value)}}/> //defining onChange function
+          </label>
+          <label htmlFor = "animal">
+              Animal 
+              <select
+                id = "animal"
+                value = {animal}
+                onChange={(e) => {updateAnimal(e.target.value)}}
+                onBlur = {(e) => {updateAnimal(e.target.value)}}
+              >
+                <option />
+                {
+                    ANIMALS.map((animal) => (
+                        <option key = {animal} value = {animal}>
+                            {animal}
+                        </option>
+                    ))
+                }
+              </select>
           </label>
           <button>Submit</button>
         </form>
